cb52f807ebaaa3522318e663d5be959036e2bd3a
skeyhd <tomplaysz123@gmail.com>
2025-09-11 02:26:02 +0100

    feat: add hello world

---
+++ brocode-tutorial/helloworld.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+
+    // This is a comment
+    /*
+        This is a
+        multi-line
+        comment
+    */
+   
+    std::cout << "I like pizza!" << '\n';
+    std::cout << "Hello world!" << '\n';
+    return 0;
+}
